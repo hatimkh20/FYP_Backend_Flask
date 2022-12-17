5791dcd093f400c46669627cbc9614cccdcca27c
hatimkh20 <hatimk51@gmail.com>
2022-12-17 17:19:18 +0500

    mongo connection added

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from flask_pymongo import PyMongo
- 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from flask_pymongo import pymongo
+
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb+srv://Categorising_in_text_Citations:#Food123@cluster0.gkdshl2.mongodb.net/?retryWrites=true&w=majority'
+CONNECTION_STRING = "mongodb+srv://Categorising_in_text_Citations:#Food123@cluster0.gkdshl2.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('flask_mongodb_atlas')
+user_collection = pymongo.collection.Collection(db, 'user_collection')
 
-mongo = PyMongo(app)
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    
-    return mongo.db.list_collection_names
- 
-# main driver function
+    db.collection.insert_one({"name": "John"})
+    print(db)
+    return "Successful"
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
