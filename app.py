945a7fd3135f76cdfdbe34c87cde81bfa511caf7
hatimkh20 <hatimk51@gmail.com>
2022-12-30 23:58:09 +0500

    added boilerplate schema

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 from flask import Flask
-import pymongo
+from mongoengine import connect
+from mongoengine.fields import *
+from mongoengine.document import Document
 
 app = Flask(__name__)
 
-CONNECTION_STRING = "mongodb+srv://Categorising_in_text_Citations:#Food123@cluster0.gkdshl2.mongodb.net/?retryWrites=truew=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
+connect(
+    db='database',
+    username='Categorising_in_text_Citations',
+    password='#Food123',
+    host='mongodb+srv://cluster0.gkdshl2.mongodb.net/'
+)
 
+class User(Document):
+    name = StringField(required=True)
+    email = EmailField()
+
+@app.route('/users')
+def user():
+    return "User found."
 
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    db.collection.insert_one({"name": "aiman"})
-    print(db)
+def create_user():
+    user = User(name='Hatim', email='john@example.com')
+    user.save()
     return "Successful"
-
 
 if __name__ == '__main__':
     app.run()
+
+
+
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('flask_mongodb_atlas')
+# user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+
+
+# @app.route('/')
+# # ‘/’ URL is bound with hello_world() function.
+# def hello_world():
+#     db.collection.insert_one({"name": "aiman"})
+#     print(db)
+#     return "Successful"
+
+
